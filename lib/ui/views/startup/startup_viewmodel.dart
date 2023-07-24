00704ddcf53d39bd991578ac3d61852446b5265d
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2023-07-24 20:29:24 +0100

    Added Firebase project

--- lib/ui/views/startup/startup_viewmodel.dart
+++ lib/ui/views/startup/startup_viewmodel.dart
@@ -1,21 +1,25 @@
 import 'package:dart_openai/dart_openai.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:logger/logger.dart';
 import 'package:travel_aigent/app/app.logger.dart';
 import 'package:travel_aigent/app/app.router.dart';
+import 'package:travel_aigent/firebase_options.dart';
 import 'package:travel_aigent/services/authentication_service.dart';
 import 'package:stacked/stacked.dart';
 import 'package:travel_aigent/app/app.locator.dart';
 import 'package:stacked_services/stacked_services.dart';
 
 class StartupViewModel extends BaseViewModel {
-  final AuthenticationService _authenticationService =
-      locator<AuthenticationService>();
+  final AuthenticationService _authenticationService = locator<AuthenticationService>();
   final NavigationService _navigationService = locator<NavigationService>();
   final Logger _logger = getLogger('StartupViewModel');
 
   Future runStartupLogic() async {
     await dotenv.load();
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
 
     /// Initialise GPT
     OpenAI.apiKey = dotenv.env['TRAVEL_AIGENT_OPEN_AI_API_KEY']!;
