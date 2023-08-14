09ac6de1f6c74f2ecb13e44106807c3fc07c8d08
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2023-08-14 20:52:29 +0100

    Fix local airport issue

--- lib/services/airport_service.dart
+++ lib/services/airport_service.dart
@@ -2,13 +2,16 @@ import 'dart:convert';
 import 'package:flutter/services.dart';
 import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 import 'package:fuzzywuzzy/model/extracted_result.dart';
+import 'package:logger/logger.dart';
 import 'package:travel_aigent/app/app.locator.dart';
+import 'package:travel_aigent/app/app.logger.dart';
 import 'package:travel_aigent/models/airport_data_model.dart';
 import 'package:travel_aigent/models/airport_model.dart';
 import 'package:travel_aigent/services/ip_service.dart';
 
 class AirportService {
   final IpService _ipService = locator<IpService>();
+  final Logger _logger = getLogger('AirportService');
 
   late AirportData airportData;
 
@@ -21,26 +24,33 @@ class AirportService {
     String userCountryCode = _ipService.ipLocation?.countryCode ?? '';
     String userCountry = _ipService.ipLocation?.country ?? '';
     String userCity = _ipService.ipLocation?.city ?? '';
+    _logger.i('countryCode: $userCountryCode - userCountry: $userCountry - userCity: $userCity');
 
     /// Get all airports that are in the same country as the user
-    localAirports.addAll(
-        airportData.airports.where((e) => e.countryIsoCode == userCountryCode));
+    localAirports.addAll(airportData.airports.where((e) => e.countryIsoCode == userCountryCode));
 
     /// If we cannot find any local airports then default to the users country
     if (localAirports.isEmpty) {
       defaultFromValue = userCountry;
+      _logger.i('cannot find local airport, defaulting to user country: $defaultFromValue');
+      return;
     }
 
-    List<String> localAirportsNames =
-        localAirports.map((e) => e.airportName).toList();
+    List<String> localAirportsNames = localAirports.map((e) => e.airportName).toList();
 
-    ExtractedResult result = extractOne(
-      query: userCity,
-      choices: localAirportsNames,
-      getter: (obj) => obj,
-    );
-
-    defaultFromValue = result.choice;
+    try {
+      ExtractedResult result = extractOne(
+        query: userCity,
+        cutoff: 60,
+        choices: localAirportsNames,
+        getter: (obj) => obj,
+      );
+      defaultFromValue = result.choice;
+      _logger.i('best airport we could find: $defaultFromValue');
+    } catch (e) {
+      defaultFromValue = userCountry;
+      _logger.i('cannot find close enough airport. defaulting to country');
+    }
   }
 
   Future<void> loadAirports() async {
