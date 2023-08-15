058667641cb1eec3d44e06444e7e0e15faa956f6
hungrymissilepod <46930094+hungrymissilepod@users.noreply.github.com>
2023-08-15 18:04:32 +0100

    Add more cloud functions

--- functions/index.js
+++ functions/index.js
@@ -249,6 +249,159 @@ exports.fetchDuckImages3 = onRequest((request, response) => {
   });
 });
 
+exports.fetchDuckImages4 = onRequest((request, response) => {
+  logger.info('FetchDuckImages4');
+  response.set('Access-Control-Allow-Origin', "*")
+  response.set('Access-Control-Allow-Methods', 'GET, POST');
+
+  /// Headers to be sent with image request
+  var headers = {
+    'authority': 'duckduckgo.com',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'sec-fetch-dest': 'empty',
+    'x-requested-with': 'XMLHttpRequest',
+    'user-agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'referer': 'https://duckduckgo.com/',
+    'accept-language': 'en-US,en;q=0.9',
+  };
+
+  logger.info('parameters: ', request.query);
+  var query = request.query.q;
+  var parameters = _parameters(query);
+  logger.info('my params: ', parameters);
+
+  var size = request.query.size;
+  var layout = request.query.layout;
+  var type = request.query.type;
+  logger.info('size: ', size);
+  logger.info('layout: ', layout);
+  logger.info('type: ', type);
+
+  logger.info('requesting a token...');
+  /// Request a token
+  axios.get(baseUrl, { params: parameters }).then(function (res) {
+    logger.info(res.data);
+    /// The token we need to make requests to get images
+    let matches = res.data.match(/vqd=([\d-]+)\&/im);
+    const token = matches[1];
+    logger.info('my token:', token);
+
+    /// These filters specify what kind of images we will get
+    const filters = `size:${size},type:${type},layout:${layout}`;
+    logger.info('filters:', filters);
+
+    /// Parameters specifing our [query], [token], and [filters]
+    var params = {
+      'l': 'us-en',
+      'o': 'json',
+      'q': query,
+      'vqd': token,
+      'f': filters,
+      'p': '1',
+      'v7exp': 'a',
+    };
+
+    logger.info('params:', params);
+
+    /// Make a request to the DuckDuckGo image service
+    var imageRequestUrl = `${baseUrl}i.js`;
+
+    logger.info('imageRequestUrl:', imageRequestUrl);
+
+    logger.info('requesting images...');
+    axios.get(imageRequestUrl, { headers: headers, params: params, timeout: 10000 }).then(function (res) {
+      logger.info(res.data);
+      response.status(200).send(res.data);
+      response.end();
+    }).catch(function (err) {
+      logger.error('failed to get images:', err);
+    });
+  }).catch(function (err) {
+    logger.error('failed to get token:', err);
+    response.sendStatus(500).end('Server Error');
+  });
+});
+
+
+exports.fetchDuckImages5 = onRequest((request, response) => {
+  logger.info('FetchDuckImages5');
+  response.set('Access-Control-Allow-Origin', "*")
+  response.set('Access-Control-Allow-Methods', 'GET, POST');
+
+  /// Headers to be sent with image request
+  var headers = {
+    'authority': 'duckduckgo.com',
+    'accept': 'application/json, text/javascript, */*; q=0.01',
+    'sec-fetch-dest': 'empty',
+    'x-requested-with': 'XMLHttpRequest',
+    'user-agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.163 Safari/537.36',
+    'sec-fetch-site': 'same-origin',
+    'sec-fetch-mode': 'cors',
+    'referer': 'https://duckduckgo.com/',
+    'accept-language': 'en-US,en;q=0.9',
+  };
+
+  logger.info('parameters: ', request.query);
+  var query = request.query.q;
+  var parameters = _parameters(query);
+  logger.info('my params: ', parameters);
+
+  var size = request.query.size;
+  var layout = request.query.layout;
+  var type = request.query.type;
+  logger.info('size: ', size);
+  logger.info('layout: ', layout);
+  logger.info('type: ', type);
+
+  logger.info('requesting a token...');
+  /// Request a token
+  axios.get(baseUrl, { params: parameters }).then(function (res) {
+    logger.info(res.data);
+    /// The token we need to make requests to get images
+    let matches = res.data.match(/vqd=([\d-]+)\&/im);
+    const token = matches[1];
+    logger.info('my token:', token);
+
+    /// These filters specify what kind of images we will get
+    const filters = `size:${size},type:${type},layout:${layout}`;
+    logger.info('filters:', filters);
+
+    /// Parameters specifing our [query], [token], and [filters]
+    var params = {
+      'l': 'us-en',
+      'o': 'json',
+      'q': query,
+      'vqd': token,
+      'f': filters,
+      'p': '1',
+      'v7exp': 'a',
+    };
+
+    logger.info('params:', params);
+
+    /// Make a request to the DuckDuckGo image service
+    var imageRequestUrl = `${baseUrl}i.js`;
+
+    logger.info('imageRequestUrl:', imageRequestUrl);
+
+    logger.info('requesting images...');
+    axios.get(imageRequestUrl, { headers: headers, params: params, timeout: 10000 }).then(function (res) {
+      logger.info(res.data);
+      response.status(200).send(res.data);
+      response.end();
+    }).catch(function (err) {
+      logger.error('failed to get images:', err);
+    });
+  }).catch(function (err) {
+    logger.error('failed to get token:', err);
+    response.sendStatus(500).end('Server Error');
+  });
+});
+
 /// Method that calls ipinfo.io and returns with it's IP info
 /// Useful for checking cloud functions ip address
 exports.whereAmI = onRequest((request, response) => {
